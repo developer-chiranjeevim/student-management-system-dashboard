a955da61cd149658f86bcf281353e920d7b82399
chiranjeevi <developer.chiranjeevim@gmail.com>
2025-06-07 12:17:50 +0530

    Create Navbar Component and Made it Responsive Across all Screens

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+
+
+const Navbar = () => {
+    return(
+        <div className="w-full bg-white px-[2rem] py-[1rem] flex items-center justify-between">
+            <div className="select-none">
+                {/* navbar logo container */}
+                <h1 className="capitalize text-[1.25rem]">welcome, back</h1>
+
+            </div>
+            <div className="">
+                {/* navbar menu grid */}
+                <div className="block md:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                        <path fillRule="evenodd" d="M3 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 5.25Zm0 4.5A.75.75 0 0 1 3.75 9h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 9.75Zm0 4.5a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Zm0 4.5a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+                    </svg>
+                </div>
+                <div className="hidden md:grid grid-cols-3 gap-[1rem] ">
+                    <div className="col-span-1 cursor-pointer mt-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                            <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+                            <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+                        </svg>
+                    </div>
+                    <div className="col-span-1 cursor-pointer mt-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                            <path fillRule="evenodd" d="M5.25 9a6.75 6.75 0 0 1 13.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 0 1-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 1 1-7.48 0 24.585 24.585 0 0 1-4.831-1.244.75.75 0 0 1-.298-1.205A8.217 8.217 0 0 0 5.25 9.75V9Zm4.502 8.9a2.25 2.25 0 1 0 4.496 0 25.057 25.057 0 0 1-4.496 0Z" clipRule="evenodd" />
+                        </svg>
+                    </div>
+
+                    <div className="col-span-1 w-[2rem] h-[2rem] rounded-full overflow-hidden cursor-pointer">
+                        <img src="https://m.economictimes.com/thumb/msid-113607812,width-1200,height-900,resizemode-4,imgsize-17336/the-batman-part-ii-heres-when-robert-pattinson-starrer-sequel-will-release.jpg" alt="" className="w-full h-full object-cover" />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+export default Navbar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Navbar from "../components/Navbar";
 
 
 const Dashboard = () => {
@@ -7,7 +7,7 @@ const Dashboard = () => {
 
     return(
         <div className="">
-            hello, world
+            <Navbar />
         </div>
     );
 };
